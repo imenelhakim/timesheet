5afefdadcad6362b17b8718b554ebff8b9087866
RaedJarboui <raedjarboui1998@gmail.com>
2021-11-10 17:14:34 +0100

    add crossOrigin to EmployeController

--- src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import tn.esprit.spring.services.IEmployeService;
 import tn.esprit.spring.services.IEntrepriseService;
 import tn.esprit.spring.services.ITimesheetService;
 
+@CrossOrigin
 @RestController
 public class RestControlEmploye {
 
